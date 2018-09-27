c9b4555414874ca2908a51bfd86a296897c5926b
Scott Ammon <scott.o.ammon@gmail.com>
2018-09-26 19:28:09 -0700

    commenting for refactor to include dweet.io on React side

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,12 @@
 import io from 'socket.io-client';
 var socket = io.connect('http://localhost:3000');
 
+
+//  include dweet.io on the front end here
+//  use listen.for function to send data
+//  to component for setting to state
+
+
 const receiveData = function(cb) {
   socket.on('sensor-data', (content) => cb(null, content));
 }
